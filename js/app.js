520e6da99eb374a84ef4009493ef1f59038aacfe
Saad151 <106397604+Saad151@users.noreply.github.com>
2022-09-13 14:50:30 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(function () {
+    //Banner Slider Activation
+    $('.banner_slider').slick({
+       prevArrow:'<i class="fa-solid fa-xl fa-angle-left banner_slider_arrow"></i>',
+       nextArrow:'<i class="fa-solid fa-xl fa-angle-right banner_slider_arrow"></i>'
+    })
+ })
